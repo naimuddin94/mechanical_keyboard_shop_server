2ae79a656413179c63f45b59682a2eb1a54e087a
naimuddin94 <developernaimuddin@gmail.com>
2024-07-09 11:49:51 +0600

    rating controller created

---
+++ src/app/modules/Rating/rating.controller.ts
@@ -0,0 +1,28 @@
+import httpStatus from 'http-status';
+import { ApiResponse, asyncHandler } from '../../utils';
+import { RatingService } from './rating.service';
+
+// Create a new rating
+const createRating = asyncHandler(async (req, res) => {
+  const result = await RatingService.saveRatingIntoDB(req);
+  res
+    .status(httpStatus.CREATED)
+    .json(
+      new ApiResponse(httpStatus.CREATED, result, 'Rating saved successfully'),
+    );
+});
+
+// Fetch all ratings
+const fetchAllRatings = asyncHandler(async (req, res) => {
+  const result = await RatingService.getAllRatingFromDB();
+  res
+    .status(httpStatus.OK)
+    .json(
+      new ApiResponse(httpStatus.OK, result, 'Rating retrieved successfully'),
+    );
+});
+
+export const RatingController = {
+  createRating,
+  fetchAllRatings,
+};
